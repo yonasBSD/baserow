f1cdb4d5362785dadcc81894f8ae62f213b584b8
Bram <bram2w@users.noreply.github.com>
2026-04-22 21:07:50 +0200

    Prepare for 2.2.1 release (#5256)

--- backend/src/baserow/config/settings/base.py
+++ backend/src/baserow/config/settings/base.py
@@ -494,7 +494,7 @@ SPECTACULAR_SETTINGS = {
         "name": "MIT",
         "url": "https://github.com/baserow/baserow/blob/develop/LICENSE",
     },
-    "VERSION": "2.2.0",
+    "VERSION": "2.2.1",
     "SERVE_INCLUDE_SCHEMA": False,
     "TAGS": [
         {"name": "Settings"},
